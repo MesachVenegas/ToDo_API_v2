1ac43481e5a03eca529568209cce395f8eabc832
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-19 12:26:19 -0700

    change: import routes to users and categories

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const userRoutes = require('./src/routes/users.routes');
+const categoryRoutes = require('./src/routes/category.routes');
+const initModels = require('./src/models/initModels');
 const db = require('./src/config/connection');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const initModels = require('./src/models/initModels');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8000;
@@ -15,18 +17,15 @@ db.authenticate()
     .then(() => console.log('Connection Data Base successfully'))
     .catch((err) => console.error(err));
 
-db.sync({ force: true })
+db.sync()
     .then(() => console.log('Sincronice Data Base successfully'))
     .catch((err) => console.error(err));
 
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
-
-app.get('/', (req, res) => {
-    console.log("servidor en linea")
-    res.send("Servidor en linea");
-})
+app.use(userRoutes);
+app.use(categoryRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server on line and listening on port ${PORT}`);
