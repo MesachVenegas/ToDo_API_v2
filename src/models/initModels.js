e0bf88203491d80201a639b11f582db72b2509fc
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-19 21:57:50 -0700

    change:  change foreignKey name

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -1,18 +1,18 @@
-const User = require('./users.model');
-const Category = require('./categories.model');
 const SubCategory = require('./subCategories.model');
+const Category = require('./categories.model');
+const User = require('./users.model');
 const Task = require('./tasks.model');
 
 const initModels = () => {
     // Una tarea pertenece a un usuario y un usuario puede tener muchas tareas(1:M).
-    Task.belongsTo(User, { foreignKey: 'user_id' });
-    User.hasMany(Task, { foreignKey: 'user_id' });
+    Task.belongsTo(User, { foreignKey: 'userId' });
+    User.hasMany(Task, { foreignKey: 'userId' });
     // Una tarea pertenece a una categoria y una categoria puede tener muchas tareas(1:M).
-    Task.belongsTo(Category, { foreignKey: 'category_id' });
-    Category.hasMany(Task, { foreignKey: 'category_id' });
+    Task.belongsTo(Category, { foreignKey: 'categoryId' });
+    Category.hasMany(Task, { foreignKey: 'categoryId' });
     // Una subcategoria pertenece a una tarea y una tarea puede tener muchas subcategorias(1:M).
-    SubCategory.belongsTo(Task, { foreignKey: 'task_id' });
-    Task.hasMany(SubCategory, { foreignKey: 'task_id' });
+    SubCategory.belongsTo(Task, { foreignKey: 'taskId' });
+    Task.hasMany(SubCategory, { foreignKey: 'taskId' });
     // varias subcategoria pertenecen a una categoria y una categoria puede tener muchas subcategorias(1:M).
     // SubCategory.belongsTo(Category, { foreignKey: 'category_id' });
     // Category.hasMany(SubCategory, { foreignKey: 'category_id' });
