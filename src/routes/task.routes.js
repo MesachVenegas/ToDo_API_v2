0349a2ec4526040e54445d06b27ae088ed9f075e
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-19 21:11:20 -0700

    create: task api routes

---
+++ src/routes/task.routes.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const { getUserTasks, createNewTask, userCompleteTask, deleteTask } = require('../controllers/tasks.controller');
+
+const routes = Router();
+
+
+routes.get('/task/:user', getUserTasks);
+
+routes.post('/task', createNewTask);
+
+routes.put('/task/:id', userCompleteTask)
+
+routes.delete('/task/:id', deleteTask)
+
+module.exports = routes;
