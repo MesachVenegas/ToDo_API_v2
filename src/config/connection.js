4f0a79c586494463a57300c8bc4b62b742ff30f1
MesachVenegas <mesach.venegas@hotmail.com>
2023-05-19 21:33:32 -0700

    change: database conection data with Entorn eviroment variables

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 const db = new Sequelize({
-    port: 5432,
-    host: 'localhost',
-    database: 'task_dev',
-    username: 'postgres',
-    password: 'root',
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     dialect: 'postgres'
 })
 
